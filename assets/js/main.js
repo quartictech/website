0540dbc20aaf0d61c8185053428d2d7fb0b593c4
Oliver Charlesworth <oliver@quartic.io>
2016-10-14 14:49:44 +0100

    Add basic Quartic copy/images/styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,7 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/map.png': 'center',
 						},
 
 					// Delay.
