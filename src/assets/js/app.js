a203acc539ae503004cd3b8e5a07f4cea347e8c6
Alex Sparrow <alex@quartic.io>
2017-02-25 14:49:51 +0000

    Add tracking to all submit buttons

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+function gaEvent(category, action, label) {
+  if (typeof ga !== 'undefined'){
+    ga('send', 'event', category, action, label);
+  }
+  else {
+    console.log("sending event", category, action, label);
+  }
+}
