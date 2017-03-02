af8094251e3bb94f3ad5b9d3ec5051948b23b770
Oliver Charlesworth <oliver@quartic.io>
2017-03-02 16:15:48 +0000

    Defer external JS where possible

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,45 +1,4 @@
 $(function() {
-  $(document).foundation();
-
-  $(".typing").typed({
-            strings: [
-                "Field operations",
-                "Manufacturing", 
-                "Infrastructure services", 
-                "Oil and gas", 
-                "Highway maintenance",
-                "Plant and equipment",
-                "Utilities networks",
-                "Aerospace",
-                "Construction",
-            ], 
-            typeSpeed: 100,
-            backDelay: 1000,
-            backSpeed: 50,
-            loop: true,
-        });
-
-        var url = $("#quartic-video").attr('src');
-        $("#quartic-video").attr('src', '');
-        $(window).on("open.zf.reveal", function() {
-            $("#quartic-video").attr('src', url);
-            gaEvent('Videos', 'play', 'Quartic intro video');
-        });
-
-        $(window).on("closed.zf.reveal", function() {
-            $("#quartic-video").attr('src', '');
-        });
+  // $(document).foundation();
 });
 
-function gaEvent(category, action, label) {
-  if (typeof ga !== 'undefined'){
-    ga('send', 'event', category, action, label);
-  }
-  else {
-    console.log("sending event", category, action, label);
-  }
-}
-
-function gaFormEvent(category, action, label, form) {
-  return gaEvent(category, action, label + " : " + $(form).serialize());
-}
